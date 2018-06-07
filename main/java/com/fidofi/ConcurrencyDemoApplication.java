ab7102c258f3908fd5557ef254b05ebeaf54e5d0
fidofi <fromwxf@gmail.com>
2018-06-07 20:32:54 +0800

    first commit

---
+++ main/java/com/fidofi/ConcurrencyDemoApplication.java
@@ -0,0 +1,12 @@
+package com.fidofi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConcurrencyDemoApplication.class, args);
+  }
+}
